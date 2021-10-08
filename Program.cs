c120c6e4776d10ab1e97eb3578597f4da3d85cd2
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-10-08 23:06:30 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPattern3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
